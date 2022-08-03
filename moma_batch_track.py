3d9f079df5f7d33f6f79d9b190d7f7e7aba9fe6e
Michael Mell <michael.mell@unibas.ch>
2022-08-03 11:49:35 +0200

    now getting the list of specified GLs

--- moma_batch_track.py
+++ moma_batch_track.py
@@ -1,37 +1,47 @@
+from glob import glob
 import yaml
 from yaml.loader import SafeLoader
 import os
 
-def get_tiff_filename(gl_path):
-    pass
-
-def build_list_of_gls(config):
+def build_list_of_gl_directory_paths(config):
     input_path = config['path']
     position = config['position']
 
-    gls=[]
+    gl_paths=[]
     for pos in position:
-        gl_path=input_path
-        # print(pos)
-        print(position[pos])
-        gl_path+=("/Pos"+str(pos))
-        # pos_name=f"Pos{position[pos]}"
-        # print(pos_name)
-        if position[pos]:
-            for gl in position[pos]['gl']:
-                gl_path+=("/Pos"+str(pos)+"_"+"GL"+str(gl))
-                # print(gl_path)
-                filename = get_tiff_filename(gl_path)
-                # print(gl)
-    pass
+        get_gl_paths_for_position(input_path, position, gl_paths, pos)
+    return gl_paths
+
+"""
+This method caluclates the paths to the GL directories.
+"""
+def get_gl_paths_for_position(input_path, position, gl_paths, pos):
+    if position[pos]: # GLs are defined for this position; iterate over them to generate list of paths
+        for gl in position[pos]['gl']:
+            gl_path=input_path
+            gl_path+=("/Pos"+str(pos))
+            gl_path+="/Pos"+str(pos)+"_"+"GL"+str(gl)
+            gl_paths.append(gl_path)
+
+def build_list_of_gl_tiff_file_paths(gl_directory_paths: list):
+    gl_tiff_paths = []
+    for path in gl_directory_paths:
+        tiff_path = glob(path+'/*[0-9].tif')[0]
+        gl_tiff_paths.append(tiff_path)
+    return gl_tiff_paths
+
 
 def __main__():
     # Open the file and load the file
-    with open('tracking_config_1.yaml') as f:
+    with open('tracking_config_test_1.yaml') as f:
         config = yaml.load(f, Loader=SafeLoader)
-        print(config)
+        # print(config)
 
-    gl_list = build_list_of_gls(config)
+    gl_directory_paths = build_list_of_gl_directory_paths(config)
+    gl_tiff_paths = build_list_of_gl_tiff_file_paths(gl_directory_paths)
+
+    # [print(path) for path in gl_directory_paths]
+    [print(path) for path in gl_tiff_paths]
 
     # input_path = config['path']
     # for res in os.walk(input_path):
--- write_yaml_config.py
+++ write_yaml_config.py
@@ -1,7 +1,9 @@
 import yaml
 
-tracking_config_1 = {
-                    'path': '/media/micha/T7/data_michael_mell/moma_test_data/000_development/feature/20220121-fix-loading-of-curated-datasets/Lis/20211026/20211026__lis__process_positions__output__20211101-013152',
+dataset_path = '/media/micha/T7/data_michael_mell/moma_test_data/000_development/feature/20220121-fix-loading-of-curated-datasets/Lis/20211026/20211026__lis__process_positions__output__20211101-013152'
+
+tracking_config_test_1 = {
+                    'path': dataset_path,
                     'position': {
                         0: {
                             'tmax': 100,
@@ -15,32 +17,32 @@ tracking_config_1 = {
                         },
                     }
 
-with open('tracking_config_1.yaml', 'w') as f:
-    data = yaml.dump(tracking_config_1, f, sort_keys=True, default_flow_style=False)
+with open('tracking_config_test_1.yaml', 'w') as f:
+    data = yaml.dump(tracking_config_test_1, f, sort_keys=True, default_flow_style=False)
 
 
 #######################
 
 
-tracking_config_2 = {
-                    'path': '/media/micha/T7/data_michael_mell/moma_test_data/000_development/feature/20220121-fix-loading-of-curated-datasets/Lis/20211026/20211026__lis__process_positions__output__20211101-013152',
+tracking_config_test_2 = {
+                    'path': dataset_path,
                     'position': {},
                     }
 
-with open('tracking_config_2.yaml', 'w') as f:
-    data = yaml.dump(tracking_config_2, f, sort_keys=True, default_flow_style=False)
+with open('tracking_config_test_2.yaml', 'w') as f:
+    data = yaml.dump(tracking_config_test_2, f, sort_keys=True, default_flow_style=False)
 
 
 #######################
 
 
-tracking_config_3 = {
-                    'path': '/media/micha/T7/data_michael_mell/moma_test_data/000_development/feature/20220121-fix-loading-of-curated-datasets/Lis/20211026/20211026__lis__process_positions__output__20211101-013152',
+tracking_config_test_3 = {
+                    'path': dataset_path,
                     'position': {},
                     }
 
-with open('tracking_config_3.yaml', 'w') as f:
-    data = yaml.dump(tracking_config_3, f, sort_keys=True, default_flow_style=False)
+with open('tracking_config_test_3.yaml', 'w') as f:
+    data = yaml.dump(tracking_config_test_3, f, sort_keys=True, default_flow_style=False)
 
 
 #######################
