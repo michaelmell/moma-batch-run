0b5610116de022b2323f4878387e04a53d05b793
Michael Mell <michael.mell@unibas.ch>
2022-08-04 14:43:02 +0200

    WIP: add logging

--- moma_batch_track.py
+++ moma_batch_track.py
@@ -1,7 +1,9 @@
 #! /usr/bin/env python
+
 from ctypes import ArgumentError
+from pathlib import Path
 import os
-import functools
+import sys
 import argparse
 from glob import glob
 
@@ -55,6 +57,9 @@ def build_list_of_command_line_arguments(config, list_of_gl_paths):
                     # cmd_args_list[ind] = build_arg_string(arg_dict)
     return cmd_args_dict_list
 
+def calculate_log_file_path(yaml_config_file_path: Path):
+    return Path(os.path.join(yaml_config_file_path.parent,yaml_config_file_path.stem + '.log'))
+
 def __main__():
     parser = argparse.ArgumentParser()
     group = parser.add_argument_group('required (mutually exclusive) arguments')
@@ -65,11 +70,35 @@ def __main__():
                     help="perform interactive curation of GLs")
     mxgroup.add_argument("-export", "--export", action='store_true',
                     help="perform headless export of tracking results")
+    parser.add_argument("-l", "--log", type=str,
+                    help="path to the log-file for this batch-run; derived from 'yaml_config_file' and stored next to it, if not specified")
     parser.add_argument("yaml_config_file", type=str,
                     help="path to YAML file with dataset configuration")
     cmd_args = parser.parse_args()
 
-    # Open the file and load the file
+    yaml_config_file_path = Path(cmd_args.yaml_config_file)
+    
+    if not yaml_config_file_path.exists():
+        print("ERROR: Check argument 'yaml_config_file'; file not found at: {yaml_config_file_path}")
+        exit(-1)
+
+    if cmd_args.log is not None:
+        log_file = Path(cmd_args.log)
+    else:
+        log_file = calculate_log_file_path(yaml_config_file_path)
+    
+    with open(log_file, 'a') as f:
+        if not f.writable():
+            if cmd_args.log is not None:
+                print("ERROR: Check argument '-log'; cannot write to the log-file at: {cmd_args.log}")
+                exit(-1)
+            else:
+                print("ERROR: Cannot write to the file log-file at: {cmd_args.log}")
+                exit(-1)
+
+    sys.stdout = open('file', 'w')
+    sys.sterr = open('file', 'w')
+
     with open(cmd_args.yaml_config_file) as f:
         config = yaml.load(f, Loader=SafeLoader)
 
@@ -91,7 +120,7 @@ def __main__():
             current_args_dict = {'headless':None, 'reload': gl_directory_path, 'analysis': analysisName}  # for running the curation we only need the GL directory path and the name of the analysis
             pass
         args_string = build_arg_string(current_args_dict)
-        moma_command = f'moma {args_string} -i {tiff_path}'
+        moma_command = f'moma {args_string} -i {tiff_path} 2>&1 | tee {log_file}'
         print(moma_command)
         os.system(moma_command)
         # os.system(f"moma --headless -p {mmproperties_path} -i {tiff} -o {output_folder}  2>&1 | tee {moma_log_file}")
