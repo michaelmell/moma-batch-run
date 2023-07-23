29a7d710c089a9a138ee3d8c0f82a9860df211f7
Michael Mell <michael.mell@unibas.ch>
2023-07-24 00:00:33 +0200

    refactor test

--- tests/moma_batch_run_test.py
+++ tests/moma_batch_run_test.py
@@ -9,7 +9,7 @@ from moma_batch_run import GlFileManager, MomaSlurmRunner, SlurmHeaderProvider
 def setup_function(function):
     print("setting up failed for: ", function)
 
-_default_header_header = """#!/bin/bash
+expected_header = """#!/bin/bash
 #SBATCH --mem=16G
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
@@ -36,7 +36,7 @@ class TestSlurmHeaderProvider():
             sut = SlurmHeaderProvider(expected)
 
     def test__slurm_header__return_correct_value(self):
-        expected = _default_header_header
+        expected = expected_header
         sut = SlurmHeaderProvider()
         assert expected == sut.slurm_header
 
@@ -50,24 +50,24 @@ class TestSlurmRunner():
     gl_file_manager = GlFileManager(path_to_gl, analysisName)
 
     expected_moma_command = f"xvfb-run moma -p /home/micha/Documents/01_work/15_moma_notes/02_moma_development/feature/20220801-implement-python-batch-processing-script/mm.properties -analysis test_analysis -headless -trackonly -i {str(path_to_gl)}/20230401_nat_iso_carbon_med3_1_MMStack__1-Pos000_GL9.tif"
-    expected_bash_script_content = f"{_default_header_header}\n{expected_moma_command}\n"
+    expected_bash_script_content = f"{expected_header}\n{expected_moma_command}\n"
     expected_slurm_script_name = "moma_slurm_script.sh"
     expected_analysis_path = Path(path_to_gl / analysisName)
     expected_analysis_track_data_path = Path(expected_analysis_path / ("track_data__" + analysisName))
     expected_bash_sript_path = Path(expected_analysis_track_data_path / expected_slurm_script_name)
 
     def test__build_moma_run_command_returns_correct_command(self):
-        sut = MomaSlurmRunner(_default_header_header)
+        sut = MomaSlurmRunner(expected_header)
         assert sut.build_moma_run_command(self.gl_file_manager, self.arg_dict) == self.expected_moma_command
 
     def test__build_slurm_bash_file_string_returns_correct_command(self):
-        sut = MomaSlurmRunner(_default_header_header)
+        sut = MomaSlurmRunner(expected_header)
         actual = sut.build_slurm_bash_file_string(self.gl_file_manager, self.arg_dict)
         assert self.expected_bash_script_content == actual
 
     def test__write_slurm_bash_script_to_analysis_folder__script_content_is_correct(self):
         self.gl_file_manager.get_gl_track_data_path().mkdir(parents=True, exist_ok=True)
-        sut = MomaSlurmRunner(_default_header_header)
+        sut = MomaSlurmRunner(expected_header)
         
         sut.write_slurm_bash_script_to_analysis_folder(self.gl_file_manager, self.arg_dict)
         
